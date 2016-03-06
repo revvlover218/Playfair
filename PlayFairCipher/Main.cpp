f9f8ae767e3ab046cd871249875725d965be918b
revvlover218 <reshalluchman@yahoo.com>
2016-03-06 16:36:54 +0200

    key_repition_1

--- PlayFairCipher/Main.cpp
+++ PlayFairCipher/Main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 int main()
 {
 	string key;
+	string keydup;
+	int length;
 
 	string alpha = "abcdefghijklmnopqrstuvwxyz";
 
@@ -14,9 +16,28 @@ int main()
 	cin >> key;
 
 	key.append(alpha);
+	length = key.length();
 
 	cout << "\n\n" << key << endl;
+	cout << "\n" << length << endl;
 
+	for (int i = 0; i < key.length(); i++)
+	{
+		for (int j = 0; j < key.length(); j++)
+		{
+			if (key[i] == key[j])
+			{
+				if (j + 1 < 26)
+				key[j] == key[j + 1];
+
+			}
+
+		}
+
+	}
+
+	cout << "\n\n" << "After removing duplicates:" << endl;
+	cout << "\n\n" << keydup << endl;
 
 
 	system("pause");
